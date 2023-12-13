b33a9b45001dd4e3f0845636caaff182d917d76e
dominikcedro <149728980+dominikcedro@users.noreply.github.com>
2023-12-13 09:08:23 +0100

    Update task7.asm
    two rotates instead of just one

--- task7.asm
+++ task7.asm
@@ -8,15 +8,19 @@
 jmp start
 .org 0x46
 
-start: ldi r16, 0xff
+start: .equ mask_nibble = 0xf
+		.equ mask_change_bit = 0b01010101
+		ldi r16, 0xff
 	   out ddrc, r16 ; ddrc is set to one, so pin c becomes OUTPUT
 	   ldi r18, 0xff
 	   ldi r16, 0x00
 	   out ddrb, r16 ; ddrb is set to 0 so pin B becomes input
 
 loop: in r16, pinb ; read a sequence representing pins
-    ori r16, 0x0f ; mask off rest of buttons (lower nibble)
+    ori r16, mask_nibble ; mask off rest of buttons (lower nibble)
 	  rol r16
+	  rol r16
+
 	  out portc, r16
 	  jmp loop
 
