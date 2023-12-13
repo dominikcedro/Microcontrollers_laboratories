5b26c023b9085a08a498b58824d18dbc299d0134
dominikcedro <149728980+dominikcedro@users.noreply.github.com>
2023-12-13 10:14:42 +0100

    Update task7.asm
    better version, more selective

--- task7.asm
+++ task7.asm
@@ -8,8 +8,13 @@
 jmp start
 .org 0x46
 
-start: .equ mask_nibble = 0xf
-		.equ mask_change_bit = 0b01010101
+start: .equ mask_nibble = 0x0f
+		.equ bit7 = 0b01111111
+		.equ bit6 = 0b10111111
+		.equ bit5 = 0b11011111
+		.equ bit4 = 0b11101111
+		.equ empty = 0xff
+		.equ inccrt_msg = 0b10011001
 		ldi r16, 0xff
 	   out ddrc, r16 ; ddrc is set to one, so pin c becomes OUTPUT
 	   ldi r18, 0xff
@@ -17,11 +22,37 @@ start: .equ mask_nibble = 0xf
 	   out ddrb, r16 ; ddrb is set to 0 so pin B becomes input
 
 loop: in r16, pinb ; read a sequence representing pins
-    ori r16, mask_nibble ; mask off rest of buttons (lower nibble)
-	  rol r16
-	  rol r16
+	 mov r17, r16
+	 ori r17, mask_nibble
+	 cpi r17, bit7
+	 breq message_bit7
+	 cpi r17, bit6
+	 breq message_bit6
+	 cpi r17,bit5
+	 breq message_bit5
+	 cpi r17, bit4
+	 breq message_bit4
+	 cpi r17, empty
+	 breq message_empty
 
-	  out portc, r16
-	  jmp loop
 
-theend: jmp pc
+incorrect_msg: ldi r17,inccrt_msg
+				out portc,r17
+				jmp loop
+
+message_empty: ldi r17, 0x7f
+				out portc, r17
+				jmp loop
+
+message_bit7:ldi r17, mask_bit7
+			 out portc, r17
+			 jmp loop
+message_bit6:ldi r17, mask_bit6
+			 out portc, r17
+			 jmp loop
+message_bit5:ldi r17, mask_bit5
+			 out portc, r17
+			 jmp loop
+message_bit4:ldi r17, mask_bit4
+			 out portc, r17
+			 jmp loop
