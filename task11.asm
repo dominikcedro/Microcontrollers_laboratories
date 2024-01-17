7713fb891f32b795bacd050407dd0bb3ebe91e68
dominikcedro <149728980+dominikcedro@users.noreply.github.com>
2024-01-17 15:03:48 +0100

    Update task11.asm
    UPDATE task finished

--- task11.asm
+++ task11.asm
@@ -4,7 +4,7 @@
 ; Task 11
 ; 10.01.2024
 
-.set press = 96 ; pressure to be stored here
+
 .equ stack_b = 0x500
 .cseg
 .org 0
@@ -12,7 +12,7 @@ jmp start
 
 
 .org 0x100
-array_press: .db     0x6A, 0, 0xAF, 0, 0xC0, 0, 0x7F0, 0, 0xB2, 0, 0xA4, 0, 0x9D, 0, 0xBE, 0
+array_press: .db     95, 0, 96, 0, 107, 0, 117, 0, 127, 0, 137, 0, 147, 0, 167, 0, 185, 0
 
 start:	
 		ldi r16, high(stack_b)
@@ -21,19 +21,29 @@ start:
 		out SPL, r16
 		ldi r30,low(array_press * 2) ;begg add_low program memory
 		ldi r31,high(array_press*2) ;begg add_low program memory
+
 call check_button
-; now r17 is the number in binary
-multiply_bin: lsl r17
+call get_index
+call check_show
+jmp start
+
+
+.org 0x150
+get_index:   ; this subroutine finds element in array in program mem
+			 ; hence every second element is 0 r17 should be prepared
+			 ;
+			 ;ARGS: r17 - binary num of button
+			 ;RETURNS: r16 - array element of position 
+				  lsl r17
 
 loop_find_element: dec r17
-					dec r17
-					lpm r16, z+
+				   dec r17
+				   lpm r16, z+
 				   
 				   cpi r17, 0
 				   brne loop_find_element
-ldi r16, press		
-call check_show
-jmp pc
+ret
+
 
 .org 0x200
 check_button:; this subroutine check which button is clicked and returns
@@ -57,7 +67,6 @@ check_show:; this subroutine checks the value of r16
 			;ARGS: r16 - value of pressure
 			;RETURNS: message on PORTC
 			.set floor = 96
-			.set ceeil = 185
 			ldi r18, 0xff
 			out ddrc, r18
 					
@@ -65,9 +74,6 @@ check_show:; this subroutine checks the value of r16
 cmp_floor: cpi r16, floor
 		   brlo mess_under; tutaj insert message border low
 
-cmp_ceil: ldi r17, ceeil
-		  cp r17,r16
-		  brlo mess_above; if r17 is lower than r16, r16 must be higher than ceeil border show message border high
 
 cmp_ceil1: .set ceeil = 106
 		  cpi r16, ceeil
@@ -81,23 +87,28 @@ cmp_ceil4:
 		  cpi r16, ceeil
 		  brlo mess3; if r16 is higher than 96 and lower than 126 than it must be in the 3 range
 cmp_ceil5:
-		  .set ceeil = 126
+		  .set ceeil = 136
 		  cpi r16, ceeil
 		  brlo mess4; if r16 is higher than 96 and lower than 106 than it must be in the 4 range
 cmp_ceil6:
-		  .set ceeil = 136
+		  .set ceeil = 146
 		  cpi r16, ceeil
 		  brlo mess5; if r16 is higher than 96 and lower than 106 than it must be in the 5 range
 cmp_ceil7:
-		  .set ceeil = 146
-		  cpi r16, ceeil
-		  brlo mess6; if r16 is higher than 96 and lower than 106 than it must be in the 6 range
-cmp_ceil8:
 		  .set ceeil = 166
 		  cpi r16, ceeil
-		  brlo mess7; if r16 is higher than 96 and lower than 106 than it must be in the 7 range
+		  brlo mess6; if r16 is higher than 96 and lower than 106 than it must be in the 6 range
+
+cmp_ceil: .set ceeil = 186
+			ldi r17, ceeil
+		  cp r17,r16
+		  brlo mess_above; if r17 is lower than r16, r16 must be higher than ceeil border show message border high
+
 
 ; messages 
+mess_under_above: ldi r20, 0b11111100 ;message when under the above border
+			out portc, r20
+			jmp finish
 
 mess_under: ldi r20, 0b01111111 ;message when under the range
 			out portc, r20
@@ -124,10 +135,6 @@ mess5:	ldi r20, 0b11110011 ;
 			jmp finish
 
 mess6:ldi r20, 0b11111001 ;
-			out portc, r20
-			jmp finish
-
-mess7:ldi r20, 0b11111100 ;
 			out portc, r20
 			jmp finish
 
